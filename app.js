e3094deb9ce566a4b011a453b3d1d229ef2ed0e5
Zach Johnson <zachtjohnson01@gmail.com>
2018-02-22 08:34:42 -0600

    updated app.js file with express lesson

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => res.sendStatus('Hello World!'));
+app.get('/', (req,res) => 
+    res.sendStatus('Hello World!')
+);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// app.METHOD(PATH, HANDLER);
+
+app.get('/', (req,res) => 
+    res.send('Hello World!')
+);
+
+app.post('/', (req,res) =>
+    res.send('Got a POST request')
+);
+
+app.put('/user', (req,res) =>
+    res.send('Got a PUT request at /user')
+);
+
+app.delete('/user', (req,res) =>
+    res.send('Got a DELETE request a /user')
+);
+
+app.listen(3000, () => 
+    console.log('Example app listening on port 3000!')
+);
